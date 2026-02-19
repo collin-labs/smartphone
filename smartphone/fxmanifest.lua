832b0a92e573f25b9491b5fce6a34a79c055ff11
collin-labs <solucoes.digitais.ecommerce@gmail.com>
2026-02-18 23:22:57 -0300

    app.js e fxmanifest.lua

--- smartphone/fxmanifest.lua
+++ smartphone/fxmanifest.lua
@@ -18,6 +18,116 @@ ui_page 'index.html'
 
 files {
     'index.html',
+    'config.json',
+
+    -- App (front-end)
     'v3/app.js',
-    'v3/styles.css'
+    'v3/styles.css',
+
+    -- Fonts
+    'fonts/fonts.css',
+    'fonts/SF-Pro-Bold.otf',
+    'fonts/SF-Pro-Light.otf',
+    'fonts/SF-Pro-Regular.otf',
+    'fonts/SF-Pro-Semibold.otf',
+    'fonts/SF-Pro-Thin.otf',
+    'fonts/SofiaPro-Black.otf',
+    'fonts/SofiaPro-Bold.otf',
+    'fonts/SofiaPro-Medium.otf',
+    'fonts/SofiaPro-Regular.otf',
+    'fonts/fa-thin-100.woff2',
+
+    -- App icons
+    'apps/bdc.svg',
+    'apps/blaze.svg',
+    'apps/blaze.webp',
+    'apps/calculator.webp',
+    'apps/cpbank.svg',
+    'apps/facetime.webp',
+    'apps/fleeca.webp',
+    'apps/ifood.webp',
+    'apps/instagram.jpg',
+    'apps/instagram_hand.png',
+    'apps/minesweeper.webp',
+    'apps/notes.webp',
+    'apps/nubank.webp',
+    'apps/olx.png',
+    'apps/paypal.webp',
+    'apps/phone.png',
+    'apps/photos.webp',
+    'apps/settings.png',
+    'apps/sms.webp',
+    'apps/tinder.webp',
+    'apps/tor.jpg',
+    'apps/truco.webp',
+    'apps/twitter.png',
+    'apps/uber.webp',
+    'apps/waze.webp',
+    'apps/weazel.webp',
+    'apps/whatsapp.jpg',
+    'apps/yellowpages.webp',
+
+    -- Stock: sons
+    'stock/dial.mp3',
+    'stock/notification.mp3',
+    'stock/photo.ogg',
+    'stock/ring.mp3',
+
+    -- Stock: imagens gerais
+    'stock/fleeca.png',
+    'stock/maps.jpg',
+    'stock/picpay.svg',
+    'stock/twitter_egg.png',
+    'stock/user.jpg',
+    'stock/user.svg',
+    'stock/user_square.png',
+
+    -- Stock: banks
+    'stock/banks/2BHyIED.jpg',
+    'stock/banks/bb.png',
+    'stock/banks/itau.png',
+    'stock/banks/nutransf.jpg',
+
+    -- Stock: cases
+    'stock/cases/intense.png',
+    'stock/cases/iphone14pro.png',
+    'stock/cases/iphonex.png',
+    'stock/cases/iphonexs.png',
+    'stock/cases/redminote8.png',
+    'stock/cases/s20.png',
+    'stock/cases/s20b.png',
+    'stock/cases/s20black.png',
+    'stock/cases/s20cda.png',
+    'stock/cases/s9.png',
+    'stock/cases/snote20.png',
+
+    -- Stock: tinder
+    'stock/tinder/chat.png',
+    'stock/tinder/dislike.svg',
+    'stock/tinder/flame.png',
+    'stock/tinder/heart.png',
+    'stock/tinder/heartbreak.svg',
+    'stock/tinder/like.png',
+    'stock/tinder/logo.svg',
+    'stock/tinder/redo.png',
+    'stock/tinder/refuse.png',
+    'stock/tinder/star.png',
+    'stock/tinder/user.png',
+
+    -- Stock: wallpapers
+    'stock/wallpapers/default.webp',
+    'stock/wallpapers/firewatch.webp',
+    'stock/wallpapers/iphone14.webp',
+    'stock/wallpapers/iphone1444.webp',
+    'stock/wallpapers/iphonex.webp',
+    'stock/wallpapers/moon.webp',
+    'stock/wallpapers/mtfuji.webp',
+    'stock/wallpapers/s20.webp',
+    'stock/wallpapers/s9.webp',
+    'stock/wallpapers/taiwan.webp',
+    'stock/wallpapers/vaporwave.webp',
+
+    -- Stock: whatsapp backgrounds
+    'stock/whatsapp/bg-dark.jpg',
+    'stock/whatsapp/bg-light.jpg'
 }
--- smartphone/v3/app.js
+++ smartphone/v3/app.js
@@ -3931,7 +3931,7 @@ return new Proxy({}, {
 })
 }
 const xo = vo("client"),
-yo = e => ".." + e.replace("/projects/smartphone", ""),
+yo = e => "." + e.replace("/projects/smartphone", ""),
 ko = new ys;
 ko.setMaxListeners(300);
 const wo = {
@@ -3997,9 +3997,9 @@ settings: Ze({
     case: "iphonexs",
     allowUnsafeURL: !1,
     uploadServer: "",
-    ringSound: "../stock/ring.mp3",
-    dialSound: "../stock/dial.mp3",
-    notificationSound: "../stock/notification.mp3",
+    ringSound: "./stock/ring.mp3",
+    dialSound: "./stock/dial.mp3",
+    notificationSound: "./stock/notification.mp3",
     instagramLogo: null != (e = globalThis.instagramLogo) ? e : yo("/projects/smartphone/stock/instagram_hand.webp"),
     blocks: [],
     currency: "R$"
@@ -8121,7 +8121,7 @@ return wl(), _l("div", ch, [l.android ? (wl(), _l(c, {
 }, [Pl("img", {
     class: "border rounded-lg",
     onLoad: t[13] || (t[13] = (...e) => l.ajustSize && l.ajustSize(...e)),
-    src: "../stock/maps.jpg",
+    src: "./stock/maps.jpg",
     alt: ""
 }, null, 32), Pl("div", _h, [Ah, Pl("span", null, g(e.$filters.vdist2(l.location, n.location)), 1)])], 8, ["onClick"])) : Ml("", !0), e.$filters.isAudio(n.content) ? (wl(), _l(u, {
     key: 3,
@@ -8162,7 +8162,7 @@ return wl(), _l("div", ch, [l.android ? (wl(), _l(c, {
 }, [l.recording ? (wl(), _l("i", $h)) : (wl(), _l("i", jh))])]))])
 }));
 rh.render = Fh, rh.__scopeId = "data-v-9611d5b8";
-const zh = rt("../stock/user.jpg"),
+const zh = rt("./stock/user.jpg"),
 Bh = rt(""),
 Hh = Ze([Ao.identity.phone]),
 qh = {
@@ -8208,7 +8208,7 @@ qh = {
                 const t = Hh.filter(((e, t) => t));
                 Ao.backend.wpp_createGroup(Bh.value.trim(), zh.value, t).then((() => {
                     l.back()
-                })), zh.value = "../stock/user.jpg", Bh.value = "", Hh.length = 0, Hh.push(Ao.identity.phone)
+                })), zh.value = "./stock/user.jpg", Bh.value = "", Hh.length = 0, Hh.push(Ao.identity.phone)
             }
         }
     }
@@ -8987,7 +8987,7 @@ return wl(), _l(o, {
             onClick: e => l.setLocation(n.location),
             class: "w-full rounded",
             onLoad: t[4] || (t[4] = (...e) => l.ajustSize && l.ajustSize(...e)),
-            src: "../stock/maps.jpg"
+            src: "./stock/maps.jpg"
         }, null, 40, ["onClick"])) : Ml("", !0), Pl("p", Ab, [Pl("span", Sb, g(n.content), 1), Pl("span", Tb, g(e.$filters.unixToHHMM(null != (a = n.created_at) ? a : Date.now())), 1)])], 2)
     })), 128))], 2)], 2)])))), 128))])),
     _: 1
@@ -9207,7 +9207,7 @@ profile: Ze(Ao.localhost ? {
     username: "jesteriruka",
     bio: "aaaaaaaaaaaaaaaaaaa",
     verified: 1,
-    avatarURL: "../stock/user.jpg"
+    avatarURL: "./stock/user.jpg"
 } : {}),
 stories: rt([]),
 storiesSeen: {},
@@ -16996,6 +16996,6 @@ return t.mount = e => {
     return l instanceof Element && (l.removeAttribute("v-cloak"), l.setAttribute("data-v-app", "")), s
 }, t
 })(LO);
-rM.component("AppInput", NP), rM.component("AppSelect", $P), rM.component("AppToggle", YO), rM.component("AppLoading", tM), rM.component("AppVerified", lM), rM.use(_O), rM.config.globalProperties.$filters = Gs, rM.config.globalProperties.$asset = (e, t) => Ao.settings[t] || ".." + e.replace("/projects/smartphone", ""), Object.defineProperty(rM.config.globalProperties, "$currency", {
+rM.component("AppInput", NP), rM.component("AppSelect", $P), rM.component("AppToggle", YO), rM.component("AppLoading", tM), rM.component("AppVerified", lM), rM.use(_O), rM.config.globalProperties.$filters = Gs, rM.config.globalProperties.$asset = (e, t) => Ao.settings[t] || "." + e.replace("/projects/smartphone", ""), Object.defineProperty(rM.config.globalProperties, "$currency", {
 get: () => Ao.settings.currency
 }), rM.mount("#root");
